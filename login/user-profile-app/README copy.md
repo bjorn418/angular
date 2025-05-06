4e56fbcb593a2031f0d29d21584943e8f75d74a7
bjorn418 <ibjorn418@gmail.com>
2025-05-06 23:20:53 +0530

    Initial commit

---
+++ login/user-profile-app/README copy.md
@@ -0,0 +1,6 @@
+npm install -g @angular/cli
+ng version
+ng new todo-app
+cd todo-app
+ng serve
+ng generate component register  / ng g c todo
---
+++ login/user-profile-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+// <!-- npm install -g @angular/cli -->
+//  <!-- ng serve -->
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'user-profile-app';
+}
